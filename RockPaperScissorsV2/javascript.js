97745cdd6d8cf4123caf9ff8f13cc80f244c1a38
Salvador Sassatelli <salvasassa3@gmail.com>
2023-03-19 21:35:56 -0300

    Juego terminado. Faltaria decorar

--- RockPaperScissorsV2/javascript.js
+++ RockPaperScissorsV2/javascript.js
@@ -4,7 +4,10 @@ let paper = document.querySelector('.paper');
 let scissors = document.querySelector('.scissors');
 let contadorP = document.querySelector('.scorePlayer');
 let contadorB = document.querySelector('.scoreBot');
-let playAgain = document.querySelector('.finish')
+let playAgain = document.querySelector('.finish');
+let playAgainButtom = document.querySelector('.playAgain')
+let robotText = document.querySelector('.eleccionRobot')
+let wl = document.querySelector('.wl')
 
 // variables lógica juego
 let puntosPlayer = 0;
@@ -28,6 +31,16 @@ scissors.addEventListener('click', () => {
   playRound();
 });
 
+playAgainButtom.addEventListener('click', () => {
+  puntosBot = 0;
+  puntosPlayer = 0;
+  playAgain.style.display = 'none';
+  contadorP.textContent = `${puntosPlayer}`;
+  contadorB.textContent = `${puntosBot}`;
+  playAgainButtom.style.backgroundColor = 'red'
+
+});
+
 // function to play a round
 function playRound() {
   let botSelection = lista[Math.floor(Math.random() * lista.length)];
@@ -40,19 +53,33 @@ function playRound() {
           playerSelection === "paper" && botSelection === 'rock'||
           playerSelection === "scissors" && botSelection === 'paper'){
             puntosPlayer++;
+            robotText.textContent ='poronga.';
+            wl.textContent ='Ganaste';
+            wl.style.color = 'green';
            }
 
   else if(botSelection === "rock" && playerSelection === 'scissors'||
           botSelection === "paper" && playerSelection === 'rock'||
           botSelection === "scissors" && playerSelection === 'paper') {
-            puntosBot++;
+            puntosBot++
+            robotText.textContent ='masa.';
+            wl.textContent ='Perdiste'
+            wl.style.color = 'red';
   }
   
   contadorP.textContent = `${puntosPlayer}`;
   contadorB.textContent = `${puntosBot}`;
   
   // check for game over
-  if (puntosPlayer >= 3 || puntosBot >= 3) {
+  if (puntosPlayer >= 3) {
     playAgain.style.display = 'flex';
+    playAgain.style.backgroundColor = 'green'
+    playAgainButtom.style.backgroundColor = 'green'
+
+
+  }
+  if (puntosBot >= 3) {
+    playAgain.style.display = 'flex';
+    playAgain.style.backgroundColor = 'red'
   }
 }
